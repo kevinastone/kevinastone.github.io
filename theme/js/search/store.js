82236fe42e977dec65281ce4d2e18b7a0ab5faed
Kevin Stone <kevinastone@gmail.com>
2019-02-11 21:24:49 -0800

    Updates

--- theme/js/search/store.js
+++
@@ -1,8 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-
-export default createStoreWithMiddleware(reducers);
