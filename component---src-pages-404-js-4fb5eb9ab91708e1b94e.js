d86331af4657e6c7e184efb8fe4d220f01abe6d0
Kevin Stone <kevinastone@gmail.com>
2019-02-16 18:24:26 -0600

    Updates

---
+++ component---src-pages-404-js-4fb5eb9ab91708e1b94e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{231:function(t,e,n){"use strict";n.r(e);var c=n(7),u=(n(0),n(248)),o=n(247);e.default=function(){return Object(c.c)(u.a,null,Object(c.c)(o.a,{title:"404: Not found"}),Object(c.c)("h1",null,"NOT FOUND"),Object(c.c)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-4fb5eb9ab91708e1b94e.js.map
